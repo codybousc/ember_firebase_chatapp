c16a038b72e2f90a0848726cf893a91620958456
kha le & cody bouscaren <student@epicodus.com>
2015-06-17 14:45:56 -0700

    Functional ember fire intergration

--- config/environment.js
+++ config/environment.js
@@ -4,6 +4,8 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'chattychat',
     environment: environment,
+    contentSecurityPolicy: { 'connect-src': "'self' wss://*.firebaseio.com" },
+    firebase: 'https://chattychat.firebaseio.com/',
     baseURL: '/',
     locationType: 'auto',
     EmberENV: {
